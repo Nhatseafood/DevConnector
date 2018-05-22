96ecedcacce246580b9503aadb48cf6536468c2c
Nhat Nguyen <nguyenhnhat@gmail.com>
2018-05-22 12:26:15 -0500

    creating login action and set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+
 import { GET_ERRORS } from "./types";
 
 // Register User
@@ -7,6 +9,26 @@ export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
     .then(response => history.push("./login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Login Get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(response => {
+      // Save to local storage
+      const { token } = response.data;
+      //Set token to local storae
+      localStorage.setItem("jwtToken", token);
+      // Set to auth header
+      setAuthToken(token);
+    })
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
