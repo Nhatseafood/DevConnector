bf2076dbff4731d4fc891479cf541d6ad656d001
Nhat Nguyen <nguyenhnhat@gmail.com>
2018-05-23 01:29:15 -0500

    adding profile handlers & sub components

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -28,6 +28,24 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(response =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: response.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
 //Create profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
